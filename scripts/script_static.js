50718c8a5548c15a63b7a5ba57b08cc3e93d8e86
xand3rr <95112987+xand3rrx@users.noreply.github.com>
2024-03-12 14:15:38 +0530

    Update script_static.js

--- scripts/script_static.js
+++ scripts/script_static.js
@@ -1,4 +1,4 @@
-const userID = "738748102311280681"; // Change this to your Discord user ID
+const userID = "1045191859657506877"; // Change this to your Discord user ID
 
 const elements = {
 	statusBox: document.getElementById("status"),
